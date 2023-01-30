f7bb697f485c98468e181b57c2fb825c399d0d2c
Ivan Passalia <kakaroto11@live.com.ar>
2023-01-30 15:37:55 -0300

    Testing

--- 01.js
+++ 01.js
@@ -8,7 +8,7 @@ function soloNumeros(array) {
   // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings llamado 'array'.
   // Debe devolver un arreglo con solo los enteros.
   // Ej: 
-  // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
+  // soloNumeros([1, 'String', 2]) debe retornar [1, 2]
 
   // Tu código aca:
 
--- tests/01.test.js
+++ tests/01.test.js
@@ -1,10 +1,10 @@
 const soloNumeros = require('../01.js');
 
 describe('soloNumeros', function() {
-  it('should return [1, 2] for [1, "Henry", 2]', function() {
-      expect(soloNumeros([1, 'Henry', 2])).toStrictEqual([1, 2]);
+  it('should return [1, 2] for [1, "String", 2]', function() {
+      expect(soloNumeros([1, 'String', 2])).toStrictEqual([1, 2]);
   });
-  it('should return [1, 3, 2] for [1, "Soy", 3, "Henry", 2]', function() {
-    expect(soloNumeros([1,'Soy', 3, 'Henry', 2])).toStrictEqual([1, 3, 2]);
+  it('should return [1, 3, 2] for [1, "palabra", 3, "String", 2]', function() {
+    expect(soloNumeros([1,'palabra', 3, 'String', 2])).toStrictEqual([1, 3, 2]);
   });
 });
